57922f90a8b7df5491afc61bba785667c0cbb740
Manvi Tyagi <manvityagi770@gmail.com>
2021-08-12 20:02:34 +0530

    codes

--- Recursion Session 1/After creating Mantra.cpp
+++ Recursion Session 1/After creating Mantra.cpp
@@ -4,66 +4,129 @@ using namespace std;
 
 int countOfDigits(int n) {
   // base case
+  if(n==0) return 0;
 
   // recursion work
+  int small_ans = countOfDigits(n/10);
 
   // my-work
-  return 0;
+  int ans = 1+small_ans;
+
+  return ans;
+
 }
 
-void sumOfDigits(int n) {
+int sumOfDigits(int n) {
   // base case
+  if(n==0) return 0;
 
   // recursion work
+  int small_ans = sumOfDigits(n/10);
 
   // my-work
+  int ans = n%10 + small_ans;
+
+  return ans;
 }
 
+// 0 1 1 2 3 5 8 13 21...
 int nthFibonacci(int n) {
   // base case
+  if(n == 0 || n == 1) return n;
 
   // recursion work
+  int prev_fib = nthFibonacci(n-2);
+  int prevprev_fib = nthFibonacci(n-1);
 
   // my-work
+  int ans = prev_fib + prevprev_fib;
 
-  return 0;
+  return ans;
+}
+/*
+
+f(n) = f(n-1) + f(n-2)
+
+tn = tn-1 + tn-2 , tn-1~tn-2
+tn = 2*tn-1 
+               , tn-1 = tn-2+tn-3 = 2*tn-2
+tn = 2*2*tn-2
+                , tn-2 = tn-3+tn-4 = 2*tn-3
+tn = 2*2*2*tn-3
+
+                 n-k = 0
+                 n = k 
+tn = 2^k*tn-k     
+tn = 2^n       
+
+*/
+
+
+//D E SI R E
+//e + rised //o(n)
+
+//TC = no. of recursive calls * time taken in 1 call
+
+void reverseString(string &s, int lo, int hi){
+ // base case
+  if(lo >= hi) return;
+
+  // recursion work
+  reverseString(s,lo+1,hi-1);
+
+  // my-work
+  swap(s[lo],s[hi]);
 }
 
 void reverseString(string &s) {
-  // base case
-
-  // recursion work
-
-  // my-work
+  reverseString(s,0,s.size()-1);
 }
 
-bool isPalindrome(string &s){
+//NITIN = NITIN
+//DESIRE != DESIRE
+//MOMO != OMOM
+bool isPalindrome(string &s, int lo, int hi){
+  if(lo >= hi) return 1;
+
+  if(s[lo] == s[hi] && isPalindrome(s,lo+1,hi-1)) return 1;
+
   return 0;
 }
 
+bool isPalindrome(string &s){
+    return isPalindrome(s,0,s.size()-1);
+}
 
-void removeDuplicates() {
+//aabbdddef = abdef
+// TC = n*n
+string removeDuplicates(string s) {
   // base case
+  if(s.size() <= 1) return s;
 
   // recursion work
+  string small_ans = removeDuplicates(s.substr(1));
 
   // my-work
+  if(s[0] == small_ans[0]) return small_ans;
+ 
+  return s[0] + small_ans;
 }
 
 int nonConsecutiveOnes(int n){
   // base case
+  if(n==1) return 2;
+  if(n==2) return 3;
 
   // recursion work
-
-  // my-work
-
-  return 0;
+  return nonConsecutiveOnes(n-1)+nonConsecutiveOnes(n-2);
 }
 
 int StairCase(int n){
   // base case
-
+  if(n < 0) return 0;
+  if(n==0) return 1;  
   // recursion work
+  return StairCase(n-1) + StairCase(n-2);
 
   // my-work
   return 0;
@@ -73,24 +136,24 @@ bool isPowerOfTwo(int n) {
      // base case
 
      // recursion work
-
+   return 0;
      // my-work
 }
 
 int main() {
     cout << "countOfDigits: " << countOfDigits(54162);
-    // cout << "\n\nsumOfDigits: " << sumOfDigits(54162);
-    // cout << "\n\nnth Fibonacci: " << nthFibonacci(6);
+    cout << "\n\nsumOfDigits: " << sumOfDigits(54162);
+    cout << "\n\nnth Fibonacci: " << nthFibonacci(6);
   
     string s = "DESIRE";
-    // cout << "\n\nOriginal String: " << s;
-    // reverseString(s);
-    // cout << "\n\nreversedString: " << s;
+    cout << "\n\nOriginal String: " << s;
+    reverseString(s);
+    cout << "\n\nreversedString: " << s;
     string s2 = "NITIN";
-    // cout << "\n\nisPalindrome: " << s << " "  << isPalindrome(s);
-    // cout << "\n\nisPalindrome: " << s2 << " " << isPalindrome(s2);
+    cout << "\n\nisPalindrome: " << s << " "  << isPalindrome(s);
+    cout << "\n\nisPalindrome: " << s2 << " " << isPalindrome(s2);
     // printSubsets("abc");
-    // cout << "\n\nremoveDuplicates: " << removeDuplicates("aabbbbccde");
+    cout << "\n\nremoveDuplicates: " << removeDuplicates("abcddcba");
     // cout << "\n\ncountNonConsecutiveOnes" << countNonConsecutiveOnes(5);
     // cout << "\n\nstairCase" << stairCase(5);
 
--- Recursion Session 1/Intro Problems.cpp
+++ Recursion Session 1/Intro Problems.cpp
@@ -2,16 +2,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print1toN(int n){ 
-
+void print1toN(int n){ // 1,2,3,4,5
+    if(n == 0) return;
+    print1toN(n-1); //1,2,3,4
+    cout << n << " "; //5
 }
 
-void printNto1(int n){
-
+void printNto1(int n){  //5 4 3 2 1
+    if(n == 0) return;
+    cout << n << " "; //5
+    printNto1(n-1);  //4 3 2 1
 }
 
+// 5! = 5*4*3*2*1
 int factorial(int n){
-    return 0;
+    if(n==0) return 1;
+    int small_ans = factorial(n-1); //4*3*2*1 = 24
+    int ans = n*small_ans;
+    return ans;
 }
 
 
@@ -19,9 +27,9 @@ int main(){
    cout << "1 to N\n";
     print1toN(5);
 
-    // cout << "\n\nN to 1\n";
-    // printNto1(5);
+    cout << "\n\nN to 1\n";
+    printNto1(5);
 
-    // cout << "\n\nFactorial\n" << factorial(5);
+    cout << "\n\nFactorial\n" << factorial(5);
     return 0;
 }
