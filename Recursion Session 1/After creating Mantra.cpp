e84ede306d4fb056c23c157b42433d14e6721012
Manvi Tyagi <manvityagi770@gmail.com>
2021-08-12 17:27:05 +0530

    Recursion Session-1

---
+++ Recursion Session 1/After creating Mantra.cpp
@@ -0,0 +1,98 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countOfDigits(int n) {
+  // base case
+
+  // recursion work
+
+  // my-work
+  return 0;
+}
+
+void sumOfDigits(int n) {
+  // base case
+
+  // recursion work
+
+  // my-work
+}
+
+int nthFibonacci(int n) {
+  // base case
+
+  // recursion work
+
+  // my-work
+
+  return 0;
+}
+
+void reverseString(string &s) {
+  // base case
+
+  // recursion work
+
+  // my-work
+}
+
+bool isPalindrome(string &s){
+  return 0;
+}
+
+
+void removeDuplicates() {
+  // base case
+
+  // recursion work
+
+  // my-work
+}
+
+int nonConsecutiveOnes(int n){
+  // base case
+
+  // recursion work
+
+  // my-work
+
+  return 0;
+}
+
+int StairCase(int n){
+  // base case
+
+  // recursion work
+
+  // my-work
+  return 0;
+}
+
+bool isPowerOfTwo(int n) {
+     // base case
+
+     // recursion work
+
+     // my-work
+}
+
+int main() {
+    cout << "countOfDigits: " << countOfDigits(54162);
+    // cout << "\n\nsumOfDigits: " << sumOfDigits(54162);
+    // cout << "\n\nnth Fibonacci: " << nthFibonacci(6);
+  
+    string s = "DESIRE";
+    // cout << "\n\nOriginal String: " << s;
+    // reverseString(s);
+    // cout << "\n\nreversedString: " << s;
+    string s2 = "NITIN";
+    // cout << "\n\nisPalindrome: " << s << " "  << isPalindrome(s);
+    // cout << "\n\nisPalindrome: " << s2 << " " << isPalindrome(s2);
+    // printSubsets("abc");
+    // cout << "\n\nremoveDuplicates: " << removeDuplicates("aabbbbccde");
+    // cout << "\n\ncountNonConsecutiveOnes" << countNonConsecutiveOnes(5);
+    // cout << "\n\nstairCase" << stairCase(5);
+
+  return 0;
+}
---
+++ Recursion Session 1/Intro Problems.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print1toN(int n){ 
+
+}
+
+void printNto1(int n){
+
+}
+
+int factorial(int n){
+    return 0;
+}
+
+
+int main(){
+   cout << "1 to N\n";
+    print1toN(5);
+
+    // cout << "\n\nN to 1\n";
+    // printNto1(5);
+
+    // cout << "\n\nFactorial\n" << factorial(5);
+    return 0;
+}
